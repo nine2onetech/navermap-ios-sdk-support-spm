ec1119495666d3a794e4e0d9adfcee718bfad310
rieul <jiwon@nine2one.io>
2023-11-16 16:30:56 +0900

    restore

--- Package.swift
+++ Package.swift
@@ -10,6 +10,15 @@ let package = Package(
         .library(name: "NMapsMap", targets: ["NMapsMap"])
     ],
     targets: [
-        .binaryTarget(name: "NMapsMap", path: "BinaryFramework/NMapsMap.xcframework")
+        .binaryTarget(
+            name: "NMapsMap",
+            url: "https://github.com/nine2onetech/navermap-ios-sdk-support-spm/releases/download/3.17.0-2/NMapsMap.xcframework.zip",
+            checksum: "e7c6ba51771bf89f740aeb6c212dd77ddde7b994f88535851bd0157c19a8c991"
+        ),
+        .binaryTarget(
+            name: "NMapsGeometry",
+            url: "https://github.com/nine2onetech/navermap-ios-sdk-support-spm/releases/download/3.17.0-2/NMapsGeometry.xcframework.zip",
+            checksum: "4116315e8653e93ae9937ec2b5571232bd4cf2f8f47027dff02bee0afad0dccd"
+        )
     ]
 )
