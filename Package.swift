a1796b3c16a2cab1e4c89d6bffd135302b82cd34
rieul <jiwon@nine2one.io>
2023-11-16 15:55:05 +0900

    add NMapsGeometry

--- Package.swift
+++ Package.swift
@@ -14,6 +14,11 @@ let package = Package(
             name: "NMapsMap",
             url: "https://github.com/nine2onetech/navermap-ios-sdk-support-spm/releases/download/3.17.0-1/NMapsMap.xcframework.zip",
             checksum: "e7c6ba51771bf89f740aeb6c212dd77ddde7b994f88535851bd0157c19a8c991"
+        ),
+        .binaryTarget(
+            name: "NMapsGeometry",
+            url: "https://github.com/nine2onetech/navermap-ios-sdk-support-spm/releases/download/3.17.0-1/NMapsGeometry.xcframework.zip",
+            checksum: "4116315e8653e93ae9937ec2b5571232bd4cf2f8f47027dff02bee0afad0dccd"
         )
     ]
 )
