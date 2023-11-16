0b3f3e5fca60b6cb6b6a408b03ba04510a3ab2d2
rieul <jiwon@nine2one.io>
2023-11-16 13:57:37 +0900

    update url

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NMapsMap",
-            url: "https://github.com/nine2onetech/navermap-ios-sdk-support-spm/releases/download/3.17.0/NMapsMap.xcframework.zip",
+            url: "https://github.com/nine2onetech/navermap-ios-sdk-support-spm/releases/download/3.17.0-1/NMapsMap.xcframework.zip",
             checksum: "e7c6ba51771bf89f740aeb6c212dd77ddde7b994f88535851bd0157c19a8c991"
         )
     ]
