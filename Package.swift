81e8b28b517398fcbb3afa30c00f04f0ea95c691
rieul <jiwon@nine2one.io>
2023-11-16 16:36:26 +0900

    update checksum

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .binaryTarget(
             name: "NMapsMap",
             url: "https://github.com/nine2onetech/navermap-ios-sdk-support-spm/releases/download/3.17.0-2/NMapsMap.xcframework.zip",
-            checksum: "e7c6ba51771bf89f740aeb6c212dd77ddde7b994f88535851bd0157c19a8c991"
+            checksum: "d4b24f7862360d5c91e2f7f12d78275291a8298a8b972ce912feff73bd0ff2d0"
         ),
         .binaryTarget(
             name: "NMapsGeometry",
